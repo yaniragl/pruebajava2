b5545151d4ba73faad0a5e79a57f7988a1ab0ec8
yaniragl <yanira_galeas@hotmail.com>
2017-11-06 18:28:28 -0300

    Creacion de la clase pruebajava2 con estrctura switch

---
+++ src/pruebajava2/Pruebajava2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava2;
+import javax.swing.JOptionPane;
+
+/**************************************************************
+ Nombre yanira Galeas Rojas
+ Fecha 06 11 2017
+ Clase Pruebajava2
+ Responsabilidad
+ * @author ceduc
+ */
+public class Pruebajava2 
+float nota = Float.parseFloat(JOptionPane.showInputDialog("Ingrese "));{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
