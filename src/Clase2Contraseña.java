19e1f6f3b6a702d292dd015dc7adb62b7a82c70d
yaniragl <yanira_galeas@hotmail.com>
2017-11-06 20:18:32 -0300

    Programa que pide contraseña

---
+++ src/Clase2Contraseña.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.swing.JOptionPane;
+/*Nombre yanira Galeas Rojas
+ Fecha 06 11 2017
+ Clase Contraseñajava2
+ Responsabilidad
+ *
+ * @author ceduc
+ */
+public class Clase2Contraseña
+{ 
+   public static void main(String[] args) 
+   {
+       
+int  contraseña, clave;
+
+while (int <=0)
+
+ 
+      
+clave = Int.parseinteger(JOptionPane.showInputDialog("Ingrese una contraseña"));
+
+if(clave <=0);
+{
+   JOptionPane.showMessageDialog(null, " cuenta bloqueada " );  
+
+       else
+       
+if(clave<=0){
+               
+       JOptionPane.showMessageDialog(null,"Comuniquese con soporte tecnico " );
+        else
+
+
+    
+}
+    
+}
+
+           
+    
